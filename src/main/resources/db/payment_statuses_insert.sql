a8fbff1af5625356d7c51355ef6a7dfe903f98ba
agorski1 <artur.gorski5@wp.pl>
2025-10-23 00:11:08 +0200

    poprawienie insertow

---
+++ src/main/resources/db/payment_statuses_insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO Payment_Statuses(PMTS_name)
+VALUES ('PENDING');
+INSERT INTO Payment_Statuses(PMTS_name)
+VALUES ('PAID');
+INSERT INTO Payment_Statuses(PMTS_name)
+VALUES ('FAILED');
+INSERT INTO Payment_Statuses(PMTS_name)
+VALUES ('REFUNDED');
---
+++ src/main/resources/db/reservation_statuses_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO Reservation_statuses (RSVS_name) VALUES ('Pending');
+INSERT INTO Reservation_statuses (RSVS_name) VALUES ('Confirmed');
+INSERT INTO Reservation_statuses (RSVS_name) VALUES ('Cancelled');
+INSERT INTO Reservation_statuses (RSVS_name) VALUES ('Completed');
